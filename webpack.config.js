54c34d8c95507683516b30299fc0a067b32372ef
mapledrive <fastmn@yahoo.com>
2017-11-07 19:39:13 +0500

    progress added

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,7 @@ const config = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env']
-        }
+        query: { presets: ['react', 'env'] }
       }
     ]
   }
