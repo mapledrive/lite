7f5e8cad429d29da3858b082ef7b0f37534686c6
mapledrive <fastmn@yahoo.com>
2017-11-15 20:19:52 +0500

    devtool

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 const config = {
   entry: "./index.js",
   output: { filename: "bundle.js" },
+  devtool: 'eval',
   module: {
     loaders: [
       {  test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['react', 'env'] }  }
