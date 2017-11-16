9e69c8a68c8bc6da008fd0bf683cd249ad214c71
mapledrive <fastmn@yahoo.com>
2017-11-17 00:54:29 +0500

    o

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const config = {
   entry: "./index.js",
   output: { filename: "bundle.js" },
   devtool: 'eval',
+  devServer: {
+	  stats: 'errors-only',
+	  open: true
+  },
   module: {
     loaders: [
       {  test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['react', 'env'] }  }
